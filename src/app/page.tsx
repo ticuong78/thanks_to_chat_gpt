203385e1d41c42c641681ee12f43ca16ef6f91a7
Cường Lê <cuongdayne17@gmail.com>
2025-10-23 18:15:27 +0700

    sua mot ti

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Footer } from "@/app/components/Footer";
 import { Header } from "@/app/components/Header";
 import { Toaster } from "@/app/components/ui/sonner";
-import { HomePage } from "@/app/home/page";
+import HomePage from "@/app/home/page";
 
 export default function Home() {
   return (
